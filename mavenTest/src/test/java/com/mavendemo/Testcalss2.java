e7f48f9ca87079c0bb0796e647bc51319b0ae33b
Test <Test@Admin.ARSSE>
2021-07-07 19:50:31 +0530

    First commit mavenTest

---
+++ mavenTest/src/test/java/com/mavendemo/Testcalss2.java
@@ -0,0 +1,17 @@
+package com.mavendemo;
+
+import org.testng.annotations.Test;
+
+public class Testcalss2 {
+
+	@Test
+	public void TC3() {
+		System.out.println("Test case 3");
+	}
+@Test	
+	public void TC4() {
+		System.out.println("Test case 4");
+	}
+
+
+}
---
+++ mavenTest/src/test/java/com/mavendemo/Testclass.java
@@ -0,0 +1,16 @@
+package com.mavendemo;
+
+import org.testng.annotations.Test;
+
+public class Testclass {
+
+	@Test
+	public void TC1() {
+		System.out.println("Test case 1");
+	}
+@Test	
+	public void TC2() {
+		System.out.println("Test case 2");
+	}
+
+}
